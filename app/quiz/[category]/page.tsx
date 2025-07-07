f509be00f806c24a8be85ff300e7a218c44abad0
LeConsulat2 <wooine2@gmail.com>
2025-07-07 21:53:40 +1200

    update [categeory] page tsx for correct prop

--- app/quiz/[category]/page.tsx
+++ app/quiz/[category]/page.tsx
@@ -2,13 +2,11 @@ import { getFortuneConfig } from '@/lib/fortune-config';
 import { notFound } from 'next/navigation';
 import { QuizClient } from './_components/quiz-client';
 
-interface QuizPageProps {
-  params: {
-    category: string;
-  };
-}
-
-export default async function QuizPage({ params }: QuizPageProps) {
+export default async function QuizPage({
+  params,
+}: {
+  params: { category: string };
+}) {
   const { category } = params;
   const config = getFortuneConfig(category);
 
