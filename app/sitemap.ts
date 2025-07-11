8e372ba76c15e427d0862cc473124e0fb1b15771
LeConsulat2 <wooine2@gmail.com>
2025-07-11 21:58:21 +1200

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
 
-const BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL || 'https://fortune-final.vercel.app';
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://yourfortune.cool';
 
 const staticPaths = [
   '/',
