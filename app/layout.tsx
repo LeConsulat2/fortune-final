258fc82f95dfa576cf2195cab32d847c63a3567d
Jonathan <wooine2@gmail.com>
2026-01-17 19:36:31 +1300

    clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,6 +61,15 @@ export default function RootLayout({
             gtag('config', 'G-QHCTQFY92K');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "v2qerdjh4y");
+          `}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-br from-amber-900 via-orange-900 to-red-900 text-gray-100`}
